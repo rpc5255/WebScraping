6d62b56461addec3329be478389df60c7227ab6c
rahul <rahul.chandran1024@gmail.com>
2020-03-05 16:36:43 -0500

    added functions to extract all of the stats tables from https://www.basketball-reference.com/leagues/NBA_2020.html into dataframes

--- basketball_reference_scraper.py
+++ basketball_reference_scraper.py
@@ -1,9 +1,9 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup, Comment
 import pandas as pd
+import re
 
-
-def get_team_stats_soup_obj():
+def get_team_stats_soup():
     url = "https://www.basketball-reference.com/leagues/NBA_2020.html"
     html = urlopen(url)
     soup = BeautifulSoup(html, "html5lib")
@@ -11,19 +11,49 @@ def get_team_stats_soup_obj():
     return BeautifulSoup(team_stats_tables, "html5lib")
 
 
-def get_team_stats_df(id):
-    team_stats_table = get_team_stats_soup_obj()
-    id_table = team_stats_table.find_all("table", id=id)
+def get_stats_df(team_stats_soup, id):
+    id_table = team_stats_soup.find_all("table", id=id)
 
-    headers_result_set = id_table[0].find_all("th", {"aria-label": True})
+    headers_result_set = id_table[0].find_all("th", {"aria-label": True, "class": re.compile('.*poptip.*')})
     headers = [th.text for th in headers_result_set][1:]
 
     data_rows = id_table[0].find_all("tr")[1:]
     data_result_set = [td.find_all("td") for td in data_rows]
+    data = [[feature.text.replace("*", "") for feature in record] for record in data_result_set]
+    return pd.DataFrame(data, columns=headers).dropna().reset_index(drop=True)
 
-    data = [[j.text.replace("*", "") for j in i] for i in data_result_set]
-    return pd.DataFrame(data, columns=headers)
+
+def get_team_stats_per_game(team_stats_soup):
+    return get_stats_df(team_stats_soup, "team-stats-per_game")
 
 
-def get_team_stats_per_game():
-    return get_team_stats_df("team-stats-per_game")
+def get_opponent_stats_per_game(team_stats_soup):
+    return get_stats_df(team_stats_soup, "opponent-stats-per_game")
+
+
+def get_season_team_stats(team_stats_soup):
+    return get_stats_df(team_stats_soup, "team-stats-base")
+
+
+def get_season_opponent_stats(team_stats_soup):
+    return get_stats_df(team_stats_soup, "opponent-stats-base")
+
+
+def get_team_stats_per_100_poss(team_stats_soup):
+    return get_stats_df(team_stats_soup, "team-stats-per_poss")
+
+
+def get_opponent_stats_per_100_poss(team_stats_soup):
+    return get_stats_df(team_stats_soup, "opponent-stats-per_poss")
+
+
+def get_miscellaneous_stats(team_stats_soup):
+    return get_stats_df(team_stats_soup, "misc_stats")
+
+
+def get_team_shooting_stats(team_stats_soup):
+    return get_stats_df(team_stats_soup, "team_shooting")
+
+
+def get_opponent_shooting_stats(team_stats_soup):
+    return get_stats_df(team_stats_soup, "opponent_shooting")
--- main.py
+++ main.py
@@ -2,7 +2,14 @@ import basketball_reference_scraper
 
 
 def main():
-    print(basketball_reference_scraper.get_team_stats_per_game())
+    team_stats_soup = basketball_reference_scraper.get_team_stats_soup()
+    basketball_reference_scraper.get_team_stats_per_game(team_stats_soup)
+    basketball_reference_scraper.get_opponent_stats_per_game(team_stats_soup)
+    basketball_reference_scraper.get_team_stats_per_100_poss(team_stats_soup)
+    basketball_reference_scraper.get_opponent_stats_per_100_poss(team_stats_soup)
+    basketball_reference_scraper.get_miscellaneous_stats(team_stats_soup)
+    basketball_reference_scraper.get_team_shooting_stats(team_stats_soup)
+    basketball_reference_scraper.get_opponent_shooting_stats(team_stats_soup)
 
 
 if __name__ == "__main__":
